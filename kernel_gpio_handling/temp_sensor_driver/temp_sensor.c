6fc659bbc215f40f296f9588a34ca839224ae5ef
Ivan Slaev <Ivan.Slaev@harman.com>
2020-04-13 19:34:30 +0300

    temp: driver: Add chardev to temp sensor for temperature export

--- kernel_gpio_handling/temp_sensor_driver/temp_sensor.c
+++ kernel_gpio_handling/temp_sensor_driver/temp_sensor.c
@@ -2,25 +2,92 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/types.h>
+#include <linux/uaccess.h>
 #include <linux/gpio.h>
+#include <linux/cdev.h>
 #include <linux/delay.h>
 #include <linux/workqueue.h>
 
+#define MYDEV "temp_cdev"
+
+static uint16_t temperature;
+static DEFINE_MUTEX (temp_mutex);
+static dev_t temp_dev;
+static struct cdev *temp_cdev;
+static struct class *temp_class;
+static unsigned int count = 1;
+
+	/* Temp sensor commands */
 #define SKIP_ROM		0xCC
 #define READ_ROM		0x33
 #define CONVERT_T		0x44
 #define READ_SCRATHPAD	0xBE
 
+	/* Temp sensor configs */
 #define PIN				4
 #define GPIO_NAME		TEMP_PIN
-#define WQ_NAME			"work_queue"
-#define WQ_HZ_DELAY		HZ/10
 
+#define WQ_NAME			"read_temperature_sensor"
 static void work_handler(struct work_struct *work);
 
 static struct workqueue_struct *led_wq;
-static DECLARE_DELAYED_WORK(led_w, work_handler);
+static DECLARE_WORK(led_w, work_handler);
 
+	/* Temp char dev */
+static int temp_open(struct inode *inode, struct file *file);
+static int temp_release(struct inode *inode, struct file *file);
+static ssize_t temp_read(struct file *file, char __user *buf, size_t lbuf, loff_t *ppos);
+
+static const struct file_operations temp_fops = {
+	.owner = THIS_MODULE,
+	.read = temp_read,
+	.open = temp_open,
+	.release = temp_release,
+};
+
+static int temp_open(struct inode *inode, struct file *file)
+{
+	static int counter = 1;
+	
+	pr_info("%s - successfully opened with MAJOR number: %d, MINOR number: %d\n", MYDEV, imajor(inode), iminor(inode));
+	pr_info("%s being opened %d times since loaded, ref=%d\n", MYDEV, counter, module_refcount(THIS_MODULE));
+	counter++;
+
+	return 0;
+}
+
+static int temp_release(struct inode *inode, struct file *file)
+{
+	pr_info("Closing %s!\n", MYDEV);
+	return 0;
+}
+
+static ssize_t temp_read(struct file *file, char __user *buf, size_t lbuf, loff_t *ppos)
+{
+	int nbytes = 0;
+	int error = 0;
+
+	if (lbuf > sizeof(uint16_t)) error--;
+
+	if (!error) {
+		int ret = queue_work(led_wq, &led_w);
+		if (!ret) {
+			pr_err("Delayed work not queued\n");
+			error--;
+		}
+	}
+
+	if (!error) {
+		mutex_lock(&temp_mutex);
+		nbytes = copy_to_user(buf, (void*)&temperature, sizeof(uint16_t));
+		pr_info("READING nbytes:%d, temperature 0x%x\n", nbytes, temperature);
+		mutex_unlock(&temp_mutex);
+	}
+
+	return nbytes;
+}
+
+	/* Temp driver logic */
 static void bus_pin_init(void)
 {
 	int ret;
@@ -56,7 +123,7 @@ static int master_reset(void)
 		
 		pr_info("DS18B20 NOT reset, try again in 420 milisecond\n");
 		udelay(480);							/* wait for the maximum DS18B20 signal time before retry reset */
-		queue_delayed_work(led_wq, &led_w, WQ_HZ_DELAY);		/* try again */
+		queue_work(led_wq, &led_w);			/* try again */
 	}
 	
 	udelay(480);
@@ -119,16 +186,17 @@ static uint64_t read_rom(void)
 	return rom;
 }	
 
-static uint16_t read_temp(void)
+static void read_temperature(void)
 {
 	int i;
-	uint16_t temp;
-	temp = 0;
+
+	mutex_lock(&temp_mutex);
+	temperature = 0;
+	for (i = 0; i < 16; i++) temperature |= (read_bit() << i);
+	pr_err("TEMPERATURE VALUE: 0x%x\n", temperature); /* raw hex value in kernel module only, not an err just for readability in dmesg */
+	mutex_unlock(&temp_mutex);
 	
-	for (i = 0; i < 16; i++) temp |= (read_bit() << i);
-	pr_err("TEMPERATURE VALUE: 0x%x\n", temp); /* raw hex value in kernel module only, not an err just for readability in dmesg */
-	
-	return temp;
+	return;
 }
 
 static void work_handler(struct work_struct *work)
@@ -150,14 +218,38 @@ static void work_handler(struct work_struct *work)
 	if (master_reset()) return;
 	write_command(SKIP_ROM);
 	write_command(READ_SCRATHPAD);
-	read_temp();
+	read_temperature();
 	if (master_reset()) return;
 }
 
-static int __init my_init(void)
+static int __init temp_init(void)
 {
 	int error = 0;
 	pr_info("Temp Module Init\n");
+
+	if (alloc_chrdev_region(&temp_dev, 0, count, MYDEV) < 0)
+	{
+		pr_info("Region allocation failed\n");
+		return -1;
+	}
+
+	if (!(temp_cdev = cdev_alloc())) 
+	{
+		pr_info("cdev_alloc() failed\n");
+		unregister_chrdev_region(temp_dev, count);
+		return -1;
+	}	
+	cdev_init(temp_cdev, &temp_fops);
+	if (cdev_add(temp_cdev, temp_dev, count) < 0) 
+	{
+		pr_info("%s\n", "cdev_add failed");
+		cdev_del(temp_cdev);
+		unregister_chrdev_region(temp_dev, count);
+		return -1;
+	}
+
+	temp_class = class_create(THIS_MODULE, "temp_class");
+	device_create (temp_class, NULL, temp_dev, "%s", "temp_cdrv");
 	
 	bus_pin_init();
 
@@ -169,29 +261,26 @@ static int __init my_init(void)
 		error--; 
 	}
 
-	if (!error) {
-		int ret = queue_delayed_work(led_wq, &led_w, WQ_HZ_DELAY);
-		if (!ret) {
-			pr_err("Delayed work not queued\n");
-			error--;
-		}
-	}
-
 	return error;
 }
 
-static void __exit my_exit(void)
+static void __exit temp_exit(void)
 {
-	cancel_delayed_work_sync(&led_w);
+	cancel_work_sync(&led_w);
 	destroy_workqueue(led_wq);
 	
 	gpio_free(PIN);
-	
+
+	device_destroy(temp_class, temp_dev);
+	class_unregister(temp_class);
+	class_destroy(temp_class);
+	unregister_chrdev_region(temp_dev, count);
+
 	pr_info("Goodbye, Temp Module Exit\n");
 }
 
-module_init(my_init);
-module_exit(my_exit);
+module_init(temp_init);
+module_exit(temp_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ivan Slaev");
